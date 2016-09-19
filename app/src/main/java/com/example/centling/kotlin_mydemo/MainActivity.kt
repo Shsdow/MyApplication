4c07303c3bdea6e251fd10cf0d575215a662584e
BadMonkey <328620988@qq.com>
2016-09-19 22:21:47 +0800

    first push code

---
+++ app/src/main/java/com/example/centling/kotlin_mydemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.centling.kotlin_mydemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    val one: Int? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
