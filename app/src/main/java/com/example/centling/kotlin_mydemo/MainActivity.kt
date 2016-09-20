c07ed6264103ed04a287309c4668c6a504169b23
BadMonkey <328620988@qq.com>
2016-09-20 23:38:56 +0800

    dev commit

--- app/src/main/java/com/example/centling/kotlin_mydemo/MainActivity.kt
+++ app/src/main/java/com/example/centling/kotlin_mydemo/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         one = add(2,5)
         two1 = add(2,512)
+        two1 = add(2,5133)
     }
 
     fun add(a: Int, b: Int): Int? = a + b
