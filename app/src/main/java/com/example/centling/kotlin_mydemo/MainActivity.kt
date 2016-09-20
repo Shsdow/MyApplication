20ff9113938ecc824b39b593967531ccb046ad88
BadMonkey <328620988@qq.com>
2016-09-20 23:50:22 +0800

    fix bug commit

--- app/src/main/java/com/example/centling/kotlin_mydemo/MainActivity.kt
+++ app/src/main/java/com/example/centling/kotlin_mydemo/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        one = add(2,5)
+        one = add(2,1235)
         two1 = add(2,512)
         two1 = add(2,5133)
     }
