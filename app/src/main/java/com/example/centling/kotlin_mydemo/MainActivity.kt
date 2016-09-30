03d4f3adf891b697bff8ce55b1a475b7760984d0
BadMonkey <328620988@qq.com>
2016-09-30 15:23:39 +0800

    add dev

--- app/src/main/java/com/example/centling/kotlin_mydemo/MainActivity.kt
+++ app/src/main/java/com/example/centling/kotlin_mydemo/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
     val two2: Int? = 1
     val two3: Int? = 1
     val two4: Int? = 1
+    val two5: Int? = 1
+    val two6: Int? = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
